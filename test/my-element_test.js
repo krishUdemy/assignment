01bd5ed24f3e40b1d45204eb886345030f9d1543
Krishnaiah Anthati <krishna.anthati@gmail.com>
2020-09-08 16:52:04 +0530

    Configuration prepared for writing unit  test cases

---
+++ test/my-element_test.js
@@ -0,0 +1,10 @@
+import {fixture, html} from '@open-wc/testing';
+
+const assert = chai.assert;
+
+suite('my-element', () => {
+
+    test('is defined', () => {
+        assert(1 === 1);
+      });
+})
